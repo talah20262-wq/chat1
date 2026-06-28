e5a2991f7b26501b561f4edbb2d7da6d934c6045
النجم <talah2026.2@gmail.com>
2026-06-28 04:53:35 +0300

    Initialize Firebase configuration in main.js
    This file initializes Firebase with the required configuration settings.

---
+++ main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCFgV_ID6n3BnIr4J1WhznAdB5iqjVm50Y",
+    authDomain: "pos-system-778dd.firebaseapp.com",
+    databaseURL: "https://pos-system-778dd.firebaseio.com",
+    projectId: "pos-system-778dd",
+    storageBucket: "pos-system-778dd.appspot.com",
+    messagingSenderId: "826306046330"
+    };
+firebase.initializeApp(config);
