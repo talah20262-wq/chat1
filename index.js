f45e15e94228ab26c657442ae1631a433ef15033
النجم <talah2026.2@gmail.com>
2026-06-28 04:53:07 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+var database=firebase.database();
+var txtuser="";
+var txtpass="";
+function SignIn(){
+  txtuser=document.getElementById('username').value+"@pos-system-778dd.appspot.com";
+  txtpass=document.getElementById('password').value;
+
+  firebase.auth().signInWithEmailAndPassword(txtuser, txtpass).catch(function(error) {
+    window.alert(error.message)
+    location.href="index.html";
+  });
+
+  firebase.auth().onAuthStateChanged(firebaseUser =>{
+    if(firebaseUser){
+      if(firebaseUser.uid=="UNXtA8yh5KN2HfgqofmUjDVMZCv1"){
+        location.href="admin.html";
+      }
+      else if(firebaseUser.uid=="mWKap68CpVRTGDcDEsOrH9kimjw2"){
+        location.href="cashier.html";
+      }
+    }
+  });
+}
+
+function SignOut()
+{
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    location.href="index.html";
+  }).catch(function(error) {
+    // An error happened.
+  });
+}
