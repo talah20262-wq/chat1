33852b4c8d7911fe5ef9044d95ad0de28e69c3f1
النجم <talah2026.2@gmail.com>
2026-06-28 05:34:29 +0300

    Update Firebase configuration details

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCFgV_ID6n3BnIr4J1WhznAdB5iqjVm50Y",
-    authDomain: "pos-system-778dd.firebaseapp.com",
-    databaseURL: "https://pos-system-778dd.firebaseio.com",
-    projectId: "pos-system-778dd",
-    storageBucket: "pos-system-778dd.appspot.com",
-    messagingSenderId: "826306046330"
-    };
+    apiKey: "AIzaSyBdJPvjmm3UUXk8mPNKBrfzjpRDYqTMnxw",
+    authDomain: "cssc-4c83c.firebaseapp.com",
+    databaseURL: "https://cssc-4c83c-default-rtdb.firebaseio.com",
+    projectId: "cssc-4c83c",
+    storageBucket: "cssc-4c83c.firebasestorage.app",
+    messagingSenderId: "633683439868",
+    appId: "1:633683439868:web:c3b91c33b1747aa6442779",
+    measurementId: "G-DW7SKD2JCD"
+};
+
 firebase.initializeApp(config);
